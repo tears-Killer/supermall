c3e3ee453a6353e9eefad5d7510d9f72198422f5
tears-Killer <913864980@qq.com>
2020-05-01 12:39:46 +0800

    tabbar封装

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
   <div id="app">
-
+  <tab-bar></tab-bar>
   </div>
 </template>
 
 <script>
+import TabBar from './components/tabbar/TabBar'
 export default {
-  name: 'vue',
+  name: "vue",
   props: {},
   data() {
     return {};
   },
   computed: {},
   methods: {},
+  components:{
+    TabBar
+  }
 };
 </script>
 
 <style scoped>
-
+@import "./assets/css/base.css";
 </style>
